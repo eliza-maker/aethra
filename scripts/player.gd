7602593ae33c926ec504855090bc8f8e870611d4
eliza-maker <elcuve08@gmail.com>
2025-03-23 20:13:14 -0500

    3

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,31 @@
 extends CharacterBody2D
 
-const SPEED = 100  # Velocidad del personaje
+@export var speed: float = 100.0
+@onready var animated_sprite = $AnimatedSprite2D
 
-func _physics_process(_delta):
+var last_direction = "front"  # Dirección inicial
+
+func _process(delta):
 	var direction = Vector2.ZERO
 
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
+		animated_sprite.play("walk_right")
+		last_direction = "right"
+	elif Input.is_action_pressed("ui_left"):
 		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
+		animated_sprite.play("walk_left")
+		last_direction = "left"
+	elif Input.is_action_pressed("ui_down"):
 		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
+		animated_sprite.play("walk_front")
+		last_direction = "front"
+	elif Input.is_action_pressed("ui_up"):
 		direction.y -= 1
+		animated_sprite.play("walk_back")
+		last_direction = "back"
+	else:
+		animated_sprite.play("idle_" + last_direction)
 
-	direction = direction.normalized()  # Evita que el personaje se mueva más rápido en diagonal
-	velocity = direction * SPEED  # Aplica la velocidad al personaje
+	velocity = direction.normalized() * speed
 	move_and_slide()
